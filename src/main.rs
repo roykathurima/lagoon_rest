92e5d35b5df622ecdb16473638d31fc653ac04ee
Roy Kathurima <kathurimaroy@gmail.com>
2025-05-04 15:53:09 +0300

    Updated the entry point to route request to our handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod handler;
 
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, routing::post};
+use tokio;
+
+use handler::make_reservation;
+
+#[tokio::main]
+async fn main() {
+    // Setup the routing
+
+    let app = Router::new().route("/reserve", post(make_reservation));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3001));
+    println!("Server running at http://{}", address);
+
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    let _ = axum::serve(listener, app).await.unwrap();
 }
