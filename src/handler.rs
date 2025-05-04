68bcfc7d61a6ff474b601fb5cbc1a74c2b0fc61d
Roy Kathurima <kathurimaroy@gmail.com>
2025-05-04 15:54:50 +0300

    Ensure that the checkin date always comes before the checkout date

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,7 @@
-use axum::Json;
+use axum::{Json, http::StatusCode, response::IntoResponse};
 use chrono::{NaiveDate, NaiveDateTime};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -23,7 +24,7 @@ pub struct Wesponse {
     estimated_confirmation_time: String,
 }
 
-pub async fn make_reservation(Json(payload): Json<RekwestPayload>) -> Json<Wesponse> {
+pub async fn make_reservation(Json(payload): Json<RekwestPayload>) -> impl IntoResponse {
     // How do we derive the response given the reqwest'
     let RekwestPayload {
         hotel_name,
@@ -34,12 +35,22 @@ pub async fn make_reservation(Json(payload): Json<RekwestPayload>) -> Json<Wespo
         reservation_time,
     } = payload;
     // TODO: Add these fields to the database just because
+    let duration = check_out_date - check_in_date;
+    let days = duration.num_days();
+    // Checkin date should always come before the checkout date;
+    // Also, you cannot check in and checkout in the same day
+    if days <= 0 {
+        // Return an error response with HTTP 400
+        let error_body = Json(json!({"error": "checkout date must come after the check in date"}));
+        return (StatusCode::BAD_REQUEST, error_body).into_response();
+    }
     Json(Wesponse {
         ref_no: Uuid::new_v4(),
         hotel_name,
-        total_amount_payable: (number_of_rooms as f32) * 50.0,
+        total_amount_payable: (number_of_rooms as f32) * 50.0 * (days as f32),
         check_in_date,
         check_out_date,
         estimated_confirmation_time: "5 hours".into(),
     })
+    .into_response()
 }
